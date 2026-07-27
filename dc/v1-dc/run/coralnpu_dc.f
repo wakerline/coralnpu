2673027c72b60424a486de06a0efa86f8e3daf59
wangyy <1017843488@qq.com>
2026-07-27 07:47:25 +0000

    ime doc

--- dc/v1-dc/run/coralnpu_dc.f
+++ dc/v1-dc/run/coralnpu_dc.f
@@ -1,6 +1,7 @@
 +define+SYNTHESIS
 +define+VLEN_128
 +define+USE_TSMC28
++define+ZVT_ON
 +incdir+/home/wangyy/002_research/coralnpu/hdl/verilog/rvv/inc
 +incdir+/home/wangyy/002_research/coralnpu/hdl/verilog/rvv/design/FPnew/common_cells/inc
-/home/wangyy/002_research/coralnpu/bazel-bin/hdl/chisel/src/coralnpu/RvvCoreMiniHighmemAxi.sv
+/home/wangyy/002_research/coralnpu/bazel-bin/hdl/chisel/src/coralnpu/VmeCoreMiniAxi.sv
